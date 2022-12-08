4434ebdd5d0429bea71c56fdf073694356d25fbd
ProphetoL <dasoures@gmail.com>
2022-12-08 17:45:18 -0500

    created parameter checking function
    abstracts away the parameter checking from each prior function to a helper. I need to check for bugs. Should I create unit tests?

--- R/btpc_exception_handling.R
+++ R/btpc_exception_handling.R
@@ -1,22 +1,59 @@
-#' Checks Prior Function Inputs
+#' Checks Prior data format
 #'
-#' Helper function to verify correct input formatting for default prior functions
+#' Check if data is correctly formatted
 #'
 #' @details
+#' @param model character, the model type
 #' @param params named vector, contains the actual params from the function call
-#' @param expectedParams char vector, contains the names of the expected params for the called model
 #' @param posteriorPredictive logical, should posterior predictive be generated insread of evaluating the deterministic model. default = FALSE
+#' @return list, input params coerced as a vector, if necessary
 
-priorConds <- function(params, expectedParams, posteriorPredictive){
+checkParams <- function(model, params, posteriorPredictive){
   if (!is.logical(posteriorPredictive)) stop('posteriorPredictive argument must be supplied as a logical (= TRUE or = FALSE)')
   if (is.null(names(params))) stop('Error in call to tpc evaluation function. param input must be named.')
+  if (!all(is.numeric(params))) stop("All parameter values must be numeric.")
+  priorNames <- names(defaultPriors(model))
+  if (!all(priorNames %in% params)){
+    missingpriors <- paste(priorNames[!(priorNames %in% params)], collapse = " ")
+    stop(paste("Missing model parameters: ", missingpriors))
+  }
+  if (!is.vector(params)){
+    warning('Expected params input to be a vector. Attempting to convert to vector')
+    params = as.vector(params)
+  }
+
+  if (posteriorPredictive && !('sigma.sq' %in% names(params))){
+    stop('Parameter "sigma.sq" is required to generate posterior predictive samples.')
+  }
+
+  return(params)
 }
 
-#' Retrieve expected parameters
-#'
-#' Retrieve expected parameters for a given thermal performance curve model
-#' @param model character, name of model type
-#'
-expectedParams <- function(model){
 
+#' Check TPC data format
+#'
+#' Check if data is correctly formatted
+#'
+#' @details This function returns a list of data and constants to be passed to a `nimble` model to perform MCMC
+#' @param data list, with expected entries "Trait" (corresponding to the trait being modeled by the thermal performance curve) and "Temp" (corresponding to the temperature in Celcius that the trait is being measured at).
+#' @return list, with entries "data" (object of class "list", data to be passed to `nimble` model) and "N" (integer number of data points, to be passed `nimble` model as a constant)
+#' @examples
+#' ## generate data
+#' test_data = list(Temp = rep(c(10, 20, 30, 40), 5), Trait = rgamma(20, 5, rep(c(10, 20, 30, 40), 5)))
+#' checkData(test_data)
+
+checkData <- function(data){
+  ## data checks to make sure there are values for Temp and Trait
+  if (!is.list(data)) stop("Unexpected class for argument 'data'. Data must be input as a list.")
+  if (is.null(unlist(data['Temp']))) stop("Data list must have a variable called 'Temp'. Perhaps check spelling and capitalization?")
+  if (is.null(unlist(data['Trait']))) stop("Data list must have a variable called 'Trait'. Perhaps check spelling and capitalization?")
+  if (!is.vector(unlist(data['Trait']))) stop('List elements of data must be numeric vectors')
+  if (!is.vector(unlist(data['Temp']))) stop('List elements of data must be numeric vectors')
+  if (length(data['Trait']) != length(data['Temp'])) stop("'Temp' and 'Trait' must have the same length.")
+  ## warnings for when temperature may be in F instead of C
+  if (any(unlist(data['Temp']) > 50)) warning('Unusual (Temp>50) temperature values detected (are Temps given in Celcius?)')
+  if (any(unlist(data['Temp']) < 0)) warning('Unusual (Temp<0) temperature values detected (are Temps given in Celcius?)')
+  if (any(is.na(data['Temp']))) warning('Temperature data contains missing values. This may lead to unexpected results')
+  if (any(is.na(data['Trait']))) warning('Trait data contains missing values. This may lead to unexpected results')
+  return(list(data = data, N = length(unlist(data['Trait']))))
 }
---
+++ R/btpc_model_evaluation.R
@@ -0,0 +1,269 @@
+#' Evaluate Ratkowsky model
+#'
+#' Evaluate Ratkowsky model for thermal performance for a given parameter set
+#'
+#' @details This function returns a numeric vector of evaluations of the Ratkowsky thermal performance curve for a reference set `Temp`
+#' @param params named vector, with entries `a`, `b`, `T.min`, `T.max`. If posteriorPredictive = TRUE, `sigma.sq` must also be present.
+#' @param Temp numeric vector, set of temperature values to evaluate the Ratkowsky model at
+#' @param posteriorPredictive logical, should posterior predictive samples be generated instead of evaluating the deterministic Ratkowsky model. default = FALSE.
+#' @return numeric vector, where entry `k` represents either the value of the Ratkowsky model at `Temp[k]` for the given vector `params` (posteriorPredictive = FALSE), or a sigle draw from the posterior predictive distribution of the Ratkowsky model at `Temp[k]` for the given vector `params` (posteriorPredictive = TRUE)
+#' @examples
+#' ## set params and reference temperature set
+#' param_set = c(T.max = 47, T.min = 10, a = .06, b = .5)
+#' Temp_ref = seq(from = 5, to = 50, length.out = 1000)
+#' plot(Temp_ref, ratkowsky_tpc(params = param_set, Temp = Temp_ref), type = 'l')
+
+
+ratkowsky_tpc <- function(params, Temp, posteriorPredictive = FALSE){
+  params = checkParams('ratkowsky', params, posteriorPredictive)
+  a = params['a']
+  b = params['b']
+  T.min = params['T.min']
+  T.max = params['T.max']
+
+  if (posteriorPredictive == FALSE){
+    curve = (Temp > T.min)*(T.max > Temp)*((a*(Temp - T.min))*(1 - exp(b*(Temp - T.max))))^2
+  } else{
+    sigma.sq = params['sigma.sq']
+
+    truncmeans = (Temp > T.min)*(T.max > Temp)*((a*(Temp - T.min))*(1 - exp(b*(Temp - T.max))))^2
+    curve = rtruncnorm(length(Temp), a = 0, b = Inf, mean = truncmeans, sd = sqrt(sigma.sq))
+  }
+  return(curve)
+}
+
+#' Evaluate Stinner model
+#'
+#' Evaluate Stinner model for thermal performance for a given parameter set
+#'
+#' @details This function returns a numeric vector of evaluations of the Stinner thermal performance curve for a reference set `Temp`
+#' @param params named vector, with entries `C`, `k1`, `k2`, `T.opt`. If posteriorPredictive = TRUE, `sigma.sq` must also be present.
+#' @param Temp numeric vector, set of temperature values to evaluate the Stinner model at
+#' @param posteriorPredictive logical, should posterior predictive samples be generated instead of evaluating the deterministic Stinner model. default = FALSE.
+#' @return numeric vector, where entry `k` represents either the value of the Stinner model at `Temp[k]` for the given vector `params` (posteriorPredictive = FALSE), or a sigle draw from the posterior predictive distribution of the Stinner model at `Temp[k]` for the given vector `params` (posteriorPredictive = TRUE)
+#' @examples
+#' ## set params and reference temperature set
+#' param_set = c(T.max = 47, T.min = 10, a = .06, b = .5)
+#' Temp_ref = seq(from = 5, to = 50, length.out = 1000)
+#' plot(Temp_ref, ratkowsky_tpc(params = param_set, Temp = Temp_ref), type = 'l')
+
+stinner_tpc <- function(params, Temp, posteriorPredictive = FALSE){
+  params = checkParams('stinner',params, posteriorPredictive)
+  C = params['C']
+  k1 = params['k1']
+  k2 = params['k2']
+  T.opt = params['T.opt']
+
+  if (posteriorPredictive == FALSE){
+    curve = C / (1 + exp(k1 + k2*(T.opt - abs(T.opt - Temp))))
+  } else{
+    sigma.sq = params['sigma.sq']
+
+    truncmeans = C / (1 + exp(k1 + k2*(T.opt - abs(T.opt - Temp))))
+    curve = rtruncnorm(length(Temp), a = 0, b = Inf, mean = truncmeans, sd = sqrt(sigma.sq))
+  }
+  return(curve)
+}
+
+#' Evaluate Kamykowski model
+#'
+#' Evaluate Kamykowski model for thermal performance for a given parameter set
+#'
+#' @details This function returns a numeric vector of evaluations of the Kamykowski thermal performance curve for a reference set `Temp`
+#' @param params named vector, with entries `a`, `b`, `c`, `T.min`, `T.max`. If posteriorPredictive = TRUE, `sigma.sq` must also be present.
+#' @param Temp numeric vector, set of temperature values to evaluate the Kamykowski model at
+#' @param posteriorPredictive logical, should posterior predictive samples be generated instead of evaluating the deterministic Ratkowsky model. default = FALSE.
+#' @return numeric vector, where entry `k` represents either the value of the Kamykowski model at `Temp[k]` for the given vector `params` (posteriorPredictive = FALSE), or a sigle draw from the posterior predictive distribution of the Kamykowski model at `Temp[k]` for the given vector `params` (posteriorPredictive = TRUE)
+#' @examples
+#' ## set params and reference temperature set
+#' param_set = c(T.max = 45, T.min = 18, a = 3, b = .1, c = 2)
+#' Temp_ref = seq(from = 5, to = 50, length.out = 1000)
+#' plot(Temp_ref, kamykowski_tpc(params = param_set, Temp = Temp_ref), type = 'l')
+
+
+kamykowski_tpc <- function(params, Temp, posteriorPredictive = FALSE){
+  params = checkParams('kamykowski', params, posteriorPredictive)
+  a = params['a']
+  b = params['b']
+  c = params['c']
+  T.min = params['T.min']
+  T.max = params['T.max']
+
+  if (posteriorPredictive == FALSE){
+    curve = (Temp > T.min)*(T.max > Temp)*a*(1 - exp(-b*(Temp - T.min)))*(1 - exp(-c*(T.max - Temp)))
+  } else{
+    sigma.sq = params['sigma.sq']
+
+    truncmeans = (Temp > T.min)*(T.max > Temp)*a*(1 - exp(-b*(Temp - T.min)))*(1 - exp(-c*(T.max - Temp)))
+    curve = rtruncnorm(length(Temp), a = 0, b = Inf, mean = truncmeans, sd = sqrt(sigma.sq))
+  }
+  return(curve)
+}
+
+#' Evaluate Schoolfield-Sharpe model
+#'
+#' Evaluate Schoolfield-Sharpe model for thermal performance for a given parameter set using the Schoolfield-Sharpe formulation from Kontopoulos et al, 2018
+#'
+#' @details This function returns a numeric vector of evaluations of the Schoolfield-Sharpe thermal performance curve for a reference set `Temp`
+#' @param params named vector, with entries `a`, `b`, `c`, `T.min`, `T.max`. If posteriorPredictive = TRUE, `sigma.sq` must also be present.
+#' @param Temp numeric vector, set of temperature values to evaluate the Schoolfield-Sharpe model at
+#' @param T.ref numeric, value for fixed parameter T.ref  used in the formulation of the Schoolfield-Sharpe model from Kontopoulos et al, 2018
+#' @param posteriorPredictive logical, should posterior predictive samples be generated instead of evaluating the deterministic Schoolfield-Sharpe model. default = FALSE.
+#' @return numeric vector, where entry `k` represents either the value of the Schoolfield-Sharpe model at `Temp[k]` for the given vector `params` (posteriorPredictive = FALSE), or a sigle draw from the posterior predictive distribution of the Schoolfield-Sharpe model at `Temp[k]` for the given vector `params` (posteriorPredictive = TRUE)
+#' @examples
+#' ## set params and reference temperature set
+#' param_set = c(T.opt = 39, e = .7, e_h = 15, r_tref = 1)
+#' Temp_ref = seq(from = 5, to = 50, length.out = 1000)
+#' plot(Temp_ref, pawar_shsch_tpc(params = param_set, Temp = Temp_ref, T.ref = 20), type = 'l')
+
+
+pawar_shsch_tpc <- function(params, Temp, T.ref, posteriorPredictive = FALSE){
+  params = checkParams('pawar-shsch', params, posteriorPredictive)
+  T.opt = params['T.opt']
+  e = params['e']
+  e_h = params['e_h']
+  r_tref = params['r_tref']
+
+  if (posteriorPredictive == FALSE){
+    curve = (e_h > e)*r_tref*exp((e/(8.62e-05))*((1/(T.ref+273.15)) - (1/(Temp + 273.15)))) / (1 + (e / (e_h-e)) * exp((e_h/(8.62e-05))*(1/(T.opt + 273.15) - 1/(Temp + 273.15))))
+  } else{
+    sigma.sq = params['sigma.sq']
+
+    truncmeans = (e_h > e)*r_tref*exp((e/(8.62e-05))*((1/(T.ref+273.15)) - (1/(Temp + 273.15)))) / (1 + (e / (e_h-e)) * exp((e_h/(8.62e-05))*(1/(T.opt + 273.15) - 1/(Temp + 273.15))))
+    curve = rtruncnorm(length(Temp), a = 0, b = Inf, mean = truncmeans, sd = sqrt(sigma.sq))
+  }
+  return(curve)
+}
+
+#' Evaluate Quadratic TPC model
+#'
+#' Evaluate Quadratic model for thermal performance for a given parameter set.
+#'
+#' @details This function returns a numeric vector of evaluations of the Quadratic thermal performance curve for a reference set `Temp`
+#' @param params named vector, with entries `q`, `T.max`, `T.min`. If posteriorPredictive = TRUE, `sigma.sq` must also be present.
+#' @param Temp numeric vector, set of temperature values to evaluate the Quadratic model at
+#' @param posteriorPredictive logical, should posterior predictive samples be generated instead of evaluating the deterministic Quadratic model. default = FALSE.
+#' @return numeric vector, where entry `k` represents either the value of the Quadratic model at `Temp[k]` for the given vector `params` (posteriorPredictive = FALSE), or a sigle draw from the posterior predictive distribution of the Schoolfield-Sharpe model at `Temp[k]` for the given vector `params` (posteriorPredictive = TRUE)
+#' @examples
+#' ## set params and reference temperature set
+#' param_set = c(T.max = 47, T.min = 23, q = .02)
+#' Temp_ref = seq(from = 5, to = 50, length.out = 1000)
+#' plot(Temp_ref, quadratic_tpc(params = param_set, Temp = Temp_ref), type = 'l')
+
+quadratic_tpc <- function(params, Temp, posteriorPredictive = FALSE){
+  params = checkParams('quadratic', params, posteriorPredictive)
+  q = params['q']
+  T.min = params['T.min']
+  T.max = params['T.max']
+
+  if (posteriorPredictive == FALSE){
+    curve = -q*(Temp - T.min)*(Temp - T.max) * (T.max > Temp) * (Temp > T.min)
+  } else{
+    sigma.sq = params['sigma.sq']
+
+    truncmeans = -q*(Temp - T.min)*(Temp - T.max) * (T.max > Temp) * (Temp > T.min)
+    curve = rtruncnorm(length(Temp), a = 0, b = Inf, mean = truncmeans, sd = sqrt(sigma.sq))
+  }
+  return(curve)
+}
+
+#' Evaluate Briere TPC model
+#'
+#' Evaluate Briere model for thermal performance for a given parameter set.
+#'
+#' @details This function returns a numeric vector of evaluations of the Briere thermal performance curve for a reference set `Temp`
+#' @param params named vector, with entries `q`, `T.max`, `T.min`. If posteriorPredictive = TRUE, `sigma.sq` must also be present.
+#' @param Temp numeric vector, set of temperature values to evaluate the Briere model at
+#' @param posteriorPredictive logical, should posterior predictive samples be generated instead of evaluating the deterministic Briere model. default = FALSE.
+#' @return numeric vector, where entry `k` represents either the value of the Briere model at `Temp[k]` for the given vector `params` (posteriorPredictive = FALSE), or a sigle draw from the posterior predictive distribution of the Schoolfield-Sharpe model at `Temp[k]` for the given vector `params` (posteriorPredictive = TRUE)
+#' @examples
+#' ## set params and reference temperature set
+#' param_set = c(T.max = 45, T.min = 20, q = .05)
+#' Temp_ref = seq(from = 5, to = 50, length.out = 1000)
+#' plot(Temp_ref, briere_tpc(params = param_set, Temp = Temp_ref), type = 'l')
+
+briere_tpc <- function(params, Temp, posteriorPredictive = FALSE){
+  params = checkParams('briere', params, posteriorPredictive)
+  q = params['q']
+  T.min = params['T.min']
+  T.max = params['T.max']
+
+  if (posteriorPredictive == FALSE){
+    curve = q*(Temp - T.min)*sqrt((T.max>Temp)*abs(T.max-Temp)) * (T.max > Temp) * (Temp > T.min)
+  } else{
+    sigma.sq = params['sigma.sq']
+
+    truncmeans = q*(Temp - T.min)*sqrt((T.max>Temp)*abs(T.max-Temp)) * (T.max > Temp) * (Temp > T.min)
+    curve = rtruncnorm(length(Temp), a = 0, b = Inf, mean = truncmeans, sd = sqrt(sigma.sq))
+  }
+  return(curve)
+}
+
+#' Evaluate Weibull TPC model
+#'
+#' Evaluate Weibull model for thermal performance for a given parameter set.
+#'
+#' @details This function returns a numeric vector of evaluations of the Weibull thermal performance curve for a reference set `Temp`
+#' @param params named vector, with entries `a`, `b`, `c`, `T.opt`. If posteriorPredictive = TRUE, `sigma.sq` must also be present.
+#' @param Temp numeric vector, set of temperature values to evaluate the Weibull model at
+#' @param posteriorPredictive logical, should posterior predictive samples be generated instead of evaluating the deterministic Weibull model. default = FALSE.
+#' @return numeric vector, where entry `k` represents either the value of the Weibull model at `Temp[k]` for the given vector `params` (posteriorPredictive = FALSE), or a sigle draw from the posterior predictive distribution of the Schoolfield-Sharpe model at `Temp[k]` for the given vector `params` (posteriorPredictive = TRUE)
+#' @examples
+#' ## set params and reference temperature set
+#' param_set = c(T.opt = 38, a = 3, b = 7e9, c = 1e9)
+#' Temp_ref = seq(from = 5, to = 50, length.out = 1000)
+#' plot(Temp_ref, weibull_tpc(params = param_set, Temp = Temp_ref), type = 'l')
+
+weibull_tpc <- function(params, Temp, posteriorPredictive = FALSE){
+  params = checkParams('weibull', params, posteriorPredictive)
+  a = params['a']
+  b = params['b']
+  c = params['c']
+  T.opt = params['T.opt']
+
+  if (posteriorPredictive == FALSE){
+    curve = ((a*(((c-1)/c)^((1-c)/c))*((((Temp-T.opt)/b)+(((c-1)/c)^(1/c)))^(c-1))*(exp(-((((Temp-T.opt)/b)+(((c-1)/c)^(1/c)))^c)+((c-1)/c)))))
+
+  } else{
+    sigma.sq = params['sigma.sq']
+
+    truncmeans = ((a*(((c-1)/c)^((1-c)/c))*((((Temp-T.opt)/b)+(((c-1)/c)^(1/c)))^(c-1))*(exp(-((((Temp-T.opt)/b)+(((c-1)/c)^(1/c)))^c)+((c-1)/c)))))
+    curve = rtruncnorm(length(Temp), a = 0, b = Inf, mean = truncmeans, sd = sqrt(sigma.sq))
+  }
+  return(curve)
+}
+
+
+#' Evaluate Gaussian TPC model
+#'
+#' Evaluate Gaussian model for thermal performance for a given parameter set.
+#'
+#' @details This function returns a numeric vector of evaluations of the Gaussian thermal performance curve for a reference set `Temp`
+#' @param params named vector, with entries `a`, `rmax`, `T.opt`. If posteriorPredictive = TRUE, `sigma.sq` must also be present.
+#' @param Temp numeric vector, set of temperature values to evaluate the Gaussian model at
+#' @param posteriorPredictive logical, should posterior predictive samples be generated instead of evaluating the deterministic Gaussian model. default = FALSE.
+#' @return numeric vector, where entry `k` represents either the value of the Gaussian model at `Temp[k]` for the given vector `params` (posteriorPredictive = FALSE), or a sigle draw from the posterior predictive distribution of the Schoolfield-Sharpe model at `Temp[k]` for the given vector `params` (posteriorPredictive = TRUE)
+#' @examples
+#' ## set params and reference temperature set
+#' param_set = c(T.opt = 36, a = 6.5, rmax = 2.75)
+#' Temp_ref = seq(from = 5, to = 50, length.out = 1000)
+#' plot(Temp_ref, gauss_tpc(params = param_set, Temp = Temp_ref), type = 'l')
+
+
+gauss_tpc <- function(params, Temp, posteriorPredictive = FALSE){
+  params = checkParams('gaussian', params, posteriorPredictive)
+  a = params['a']
+  rmax = params['rmax']
+  T.opt = params['T.opt']
+
+  if (posteriorPredictive == FALSE){
+    curve = rmax*exp(-0.5*(abs(Temp - T.opt)/a)^2)
+  } else{
+    sigma.sq = params['sigma.sq']
+
+    truncmeans = rmax*exp(-0.5*(abs(Temp - T.opt)/a)^2)
+    curve = rtruncnorm(length(Temp), a = 0, b = Inf, mean = truncmeans, sd = sqrt(sigma.sq))
+  }
+  return(curve)
+}
+
