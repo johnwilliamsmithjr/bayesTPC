9fff4bbeb08a7ca59f81c5831ec192dd16d77e23
ProphetoL <seansorek@vt.edu>
2023-02-08 11:18:50 -0500

    function that makes model evaluation functions
    the model evaluator-inator

---
+++ R/refactor_model_eval.R
@@ -0,0 +1,62 @@
+model_evaluation_function <- function(model){
+  model_info <- master_list[master_list$name == model,]
+  model_function <- function(params,
+                             Temp,
+                             posteriorPredictive = F,
+                             sigma.sq = 0){
+
+    #assign parameters into individual variables
+    params <- checkParams(model, params, F)
+    for (i in 1:length(params)){
+      assign(names(params[i]), unlist(as.vector(params[i])))
+    }
+
+    #get formula from dataframe and parse
+    evaluated_model <-
+      eval(str2expression(model_info$formula[[1]]))
+
+    #determine output
+    if (posteriorPredictive == F){
+      return(evaluated_model)
+    }
+    else{
+      curve <- rtruncnorm(length(Temp), a = 0, b = Inf,
+                          mean = evaluated_model,
+                          sd = sqrt(sigma.sq))
+      return(curve)
+    }
+  }
+
+  return(model_function)
+}
+
+#for INTERNAL USE ONLY
+.model_eval <- function(model){
+  model_info <- master_list[master_list$name == model,]
+  model_function <- function(params,
+                             Temp,
+                             posteriorPredictive = F,
+                             sigma.sq = 0){
+
+    #assign parameters into individual variables
+    #assume params is sorted lexicographically
+    sorted_vars <- sort(unlist(model_info$params))
+
+    #get formula from dataframe and parse
+    evaluated_model <-
+      eval(str2expression(model_info$formula[[1]]))
+
+    #determine output
+    if (posteriorPredictive == F){
+      return(evaluated_model)
+    }
+    else{
+      curve <- rtruncnorm(length(Temp), a = 0, b = Inf,
+                          mean = evaluated_model,
+                          sd = sqrt(sigma.sq))
+      return(curve)
+    }
+  }
+
+  return(model_function)
+}
